b52b52541e937a2b92adee81c885a5741c9e7d13
Tanmmay R Joseph <tanmmayrj@gmail.com>
2026-05-09 17:51:00 +0530

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+    },
+  },
+];
